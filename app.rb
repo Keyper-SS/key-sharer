b7e31ccf34aafc604cab35dc69bf57ca76489ccc
vicky <lch82327@gmail.com>
2016-04-14 20:55:17 +0800

    add api template

--- app.rb
+++ app.rb
@@ -14,41 +14,21 @@ class ShareKeysAPI < Sinatra::Base
     'KeyShare web service is up and running at /api/v1'
   end
 
-  get '/api/v1/keys/?' do
-    content_type 'application/json'
-    id_list = Key.all
+  get '/api/v1/accounts/?' do
+  end
 
-    { key_id: id_list }.to_json
+  get '/api/v1/accounts/:key_id.json' do
+  end
+
+  post '/api/v1/accounts/?' do
+  end
+
+  get '/api/v1/keys/?' do
   end
 
   get '/api/v1/keys/:key_id.json' do
-    content_type 'application/json'
-
-    begin
-      { key: Key.find(params[:key_id]) }.to_json
-    rescue => e
-      status 404
-      logger.info "FAILED to GET key: #{e.inspect}"
-    end
   end
 
   post '/api/v1/keys/?' do
-    content_type 'application/json'
-
-    begin
-      new_data = JSON.parse(request.body.read)
-      logger.info new_data
-      new_key = Key.new(new_data)
-      if new_key.save
-        logger.info "NEW KEY STORED: #{new_key.id}"
-      else
-        halt 400, "Could not store key: #{new_key}"
-      end
-
-      redirect '/api/v1/keys/' + new_key.id + '.json'
-    rescue => e
-      status 400
-      logger.info "FAILED to create new key: #{e.inspect}"
-    end
   end
 end
