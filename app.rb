d37b00f64f6027fcab9037fdee614a301ff7e876
yiweihuang <n1993212@gmail.com>
2016-04-14 23:39:08 +0800

    add account api

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require 'sinatra'
 require 'json'
-require 'base64'
 require_relative 'config/environments'
 require_relative 'models/init'
 
 # Configuration Sharing Web Service
 class ShareKeysAPI < Sinatra::Base
   before do
-    Key.setup
+    host_url = "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
+    @request_url = URI.join(host_url, request.path.to_s)
   end
 
   get '/?' do
@@ -15,12 +15,39 @@ class ShareKeysAPI < Sinatra::Base
   end
 
   get '/api/v1/accounts/?' do
+    content_type 'application/json'
+
+    JSON.pretty_generate(data: Account.all)
   end
 
-  get '/api/v1/accounts/:key_id.json' do
+  get '/api/v1/accounts/:account_username' do
+    content_type 'application/json'
+
+    account_username = params[:account_username]
+    account = Account.where(username: account_username).first
+    keys = account ? Account[account.id].keys : []
+
+    if account
+      JSON.pretty_generate(data: account, relationships: keys)
+    else
+      halt 404, "PROJECT NOT FOUND: #{account_username}"
+    end
   end
 
   post '/api/v1/accounts/?' do
+    begin
+      new_data = JSON.parse(request.body.read)
+      saved_account = Account.create(new_data)
+    rescue => e
+      logger.info "FAILED to create new project: #{e.inspect}"
+      halt 400
+    end
+
+    new_location = URI.join(@request_url.to_s + '/', saved_account.username.to_s)
+                      .to_s
+
+    status 201
+    headers('Location' => new_location)
   end
 
   get '/api/v1/keys/?' do
