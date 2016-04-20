27909f41288d3308a527ce4123800ac68a58e2cc
eduardo-salazar <esalazar922@gmail.com>
2016-04-20 20:49:56 +0800

    Fix Schema and models

--- db/migrations/001_accounts_create.rb
+++ db/migrations/001_accounts_create.rb
@@ -2,11 +2,13 @@ require 'sequel'
 
 Sequel.migration do
   change do
-    create_table(:accounts) do
+    create_table(:users) do
       primary_key :id
       String :username, unique: true, null: false
-      String :password, unique: false, null: false
+      String :password_encrypted, unique: false, null: false
       String :email, unique: true, null: false
+
+      set_allowed_columns :email
     end
   end
 end
--- models/account.rb
+++ models/account.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'sequel'
 
 # Holds a Account information
-class Account < Sequel::Model
+class User < Sequel::Model
   one_to_many :secrets
   many_to_many :secrets_received, :class=> :Secret , :left_key=>:receiver_id, :right_key=>:secret_id, :join_table=>:sharings
   many_to_many :secrets_shared, :class=> :Secret ,:left_key=>:sharer_id, :right_key=>:secret_id, :join_table=>:sharings
