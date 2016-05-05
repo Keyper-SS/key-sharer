143f0731eaad6f6da52ccecd88266618d68a13fc
vicky <lch82327@gmail.com>
2016-05-05 20:32:03 +0800

    change folder

--- controllers/init.rb
+++ controllers/init.rb
@@ -1,3 +1,3 @@
 require_relative 'base'
-require_relative 'secrets_controller'
-require_relative 'users_controller'
+require_relative 'users/owned_secrets'
+require_relative 'users/users_controller'
