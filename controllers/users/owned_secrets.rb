a7578c5334d3c913ea80da55174ab19cc1147248
vicky <lch82327@gmail.com>
2016-05-09 23:09:25 +0800

    change route from '/:username/secrets' to '/:username/owned_secrets'

--- controllers/users/owned_secrets.rb
+++ controllers/users/owned_secrets.rb
@@ -1,41 +1,19 @@
 # Configuration Sharing Web Service
 class ShareKeysAPI < Sinatra::Base
-  get '/api/v1/users/:username/secrets/?' do
+  get '/api/v1/users/:username/owned_secrets/?' do
     content_type 'application/json'
     username = params[:username]
     user = User.where(username: username).first
     owned_secrets = user ? User[user.id].owned_secrets : []
-    shared_secrets = user ? User[user.id].shared_secrets : []
-    received_secrets = user ? User[user.id].received_secrets : []
 
     if user
-      JSON.pretty_generate(data: user, secrets: { owned: owned_secrets,
-                                                  shared: shared_secrets,
-                                                  received: received_secrets })
+      JSON.pretty_generate(data: user, owned_secrets:  owned_secrets)
     else
       halt 404, "ACCOUNT NOT FOUND: #{username}"
     end
   end
 
-  get '/api/v1/users/:username/secrets/:secret_id/?' do
-    content_type 'application/json'
-    secret_find = nil
-    username = params[:username]
-    user = User.where(username: username).first
-
-    owned_secrets = user ? User[user.id].owned_secrets : []
-    owned_secrets.each do |secret|
-      secret_find = secret if Integer(secret.id) == Integer(params[:secret_id])
-    end
-
-    if secret_find
-      JSON.pretty_generate(data: secret_find)
-    else
-      halt 404, "SECRET NOT FOUND OR NOT AUTHORIZED TO READ: #{username}"
-    end
-  end
-
-  post '/api/v1/users/:username/secrets/?' do
+  post '/api/v1/users/:username/owned_secrets/?' do
     begin
       secret_data = JSON.parse(request.body.read)
       secret = CreateSecret.call(
